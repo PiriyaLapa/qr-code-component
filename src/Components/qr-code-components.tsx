9a2e29452bbfa61bb53ae98d1a7b59490155d601
Piriya <piriyalapa@gmail.com>
2023-11-17 11:06:40 +0700

    set initial card of qrcode

--- src/Components/qr-code-components.tsx
+++ src/Components/qr-code-components.tsx
@@ -1,12 +1,34 @@
 import { Component, ReactNode } from "react";
 import styled from "styled-components";
 
+const CardQR = styled.div`
+  border: 2px solid black;
+  width: 320px;
+  height: 497px;
+  margin: auto 0;
+  border-radius: 20px;
+`;
+
+const QrBox = styled(CardQR)`
+  width: 288px;
+  height: 288px;
+  background-color: #3685FF;
+`;
+const ContentBox = styled(QrBox)`
+  height: auto;
+  background-color: white;
+`;
+
 export default class QrCode extends Component {
   render(): ReactNode {
-    return(
+    return (
       <div>
         <h1>qr-code-component</h1>
+        <CardQR>
+          <QrBox/>
+        <ContentBox>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</ContentBox>
+        </CardQR>
       </div>
-    )
+    );
   }
 }
